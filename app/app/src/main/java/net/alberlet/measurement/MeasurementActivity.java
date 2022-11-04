eed824180072c1d6be03ae402b987aaec31e1153
CsonkaDavid <shay9505@gmail.com>
2022-11-04 18:47:18 +0100

    Mérési felület elkészítése

---
+++ app/app/src/main/java/net/alberlet/measurement/MeasurementActivity.java
@@ -0,0 +1,14 @@
+package net.alberlet.measurement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MeasurementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.measurement_activity);
+    }
+}
