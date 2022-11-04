c66ad9cc8da26d96e230077d2322d13436c4e603
CsonkaDavid <shay9505@gmail.com>
2022-11-04 18:55:47 +0100

    Adatbázis felület layout elkészítése

---
+++ app/app/src/main/java/net/alberlet/measurement/DatabaseActivity.java
@@ -0,0 +1,14 @@
+package net.alberlet.measurement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DatabaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.database_activity);
+    }
+}
