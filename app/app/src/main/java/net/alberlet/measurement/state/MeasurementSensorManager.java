e5fd00fa51f2d6a0ea4c10c3cb716220e94eb3bb
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-11-11 17:30:14 +0100

    Szenzorkezelő létrehozása

---
+++ app/app/src/main/java/net/alberlet/measurement/state/MeasurementSensorManager.java
@@ -0,0 +1,36 @@
+package net.alberlet.measurement.state;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+public class MeasurementSensorManager implements SensorEventListener {
+    private SensorManager sensorManager;
+
+    public MeasurementSensorManager() {
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            double sides = event.values[0];
+            double upDown = event.values[1];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        return;
+    }
+
+    public void setSensorManager(SensorManager sensorManager) {
+        this.sensorManager = sensorManager;
+    }
+}
