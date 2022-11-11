4a1ed201b9f6b275df1f574a1efbc4a488ba925e
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-11-11 17:36:15 +0100

    Szenzorkezelő meghívása az activity-ben

--- app/app/src/main/java/net/alberlet/measurement/MeasurementActivity.java
+++ app/app/src/main/java/net/alberlet/measurement/MeasurementActivity.java
@@ -1,14 +1,21 @@
 package net.alberlet.measurement;
 
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import net.alberlet.measurement.state.MeasurementSensorManager;
+
 public class MeasurementActivity extends AppCompatActivity {
+    private MeasurementSensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.measurement_activity);
+        sensorManager = new MeasurementSensorManager();
+        sensorManager.setSensorManager((SensorManager)getSystemService(SENSOR_SERVICE));
     }
+
 }
