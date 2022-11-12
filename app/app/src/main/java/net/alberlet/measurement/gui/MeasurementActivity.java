304e135eab88c2a0a26935d8d8bec97a9906d763
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-11-12 19:37:32 +0100

    Mérés funkcionalitás vázának felépítése

--- app/app/src/main/java/net/alberlet/measurement/gui/MeasurementActivity.java
+++ app/app/src/main/java/net/alberlet/measurement/gui/MeasurementActivity.java
@@ -2,23 +2,50 @@ package net.alberlet.measurement.gui;
 
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import net.alberlet.measurement.R;
 import net.alberlet.measurement.state.MeasurementSensorManager;
 
-public class MeasurementActivity extends AppCompatActivity {
+public class MeasurementActivity extends AppCompatActivity implements View.OnClickListener {
 
     private MeasurementSensorManager sensorManager;
 
+    private AppCompatButton measurementButton;
 
+    private TextView measurementTextView;
+
+    private boolean isMeasuring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_measurement);
         sensorManager = new MeasurementSensorManager((SensorManager)getSystemService(SENSOR_SERVICE));
+        measurementButton = findViewById(R.id.button_toggle_measurement);
+        measurementTextView = findViewById(R.id.text_view_measurement);
+        isMeasuring = false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!isMeasuring) {
+            startMeasuring();
+        } else {
+            stopMeasuring();
+        }
+    }
+
+    private void startMeasuring() {
+
+    }
+
+    private void stopMeasuring() {
+
     }
 
 }
