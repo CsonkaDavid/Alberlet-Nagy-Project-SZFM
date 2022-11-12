c2c992ebfd8f903dba40272c965b01fe679913ce
CsonkaDavid <shay9505@gmail.com>
2022-11-12 19:02:28 +0100

    Adatbázis vezérlő osztály létrehozása

---
+++ app/app/src/main/java/net/alberlet/measurement/database/MeasurementDatabase.java
@@ -0,0 +1,28 @@
+package net.alberlet.measurement.database;
+
+import android.app.Activity;
+
+import androidx.room.Room;
+
+import net.alberlet.measurement.database.entity.Measurement;
+
+import java.util.List;
+
+public class MeasurementDatabase {
+    private final AppDatabase database;
+
+    public MeasurementDatabase(Activity activity) {
+        database = Room.databaseBuilder(activity.getApplicationContext(),
+                AppDatabase.class, "").build();
+    }
+
+    private List<Measurement> getMeasurements() {
+        return database.measurementDao().getAllMeasurements();
+    }
+
+    private void insertMeasurement(String date, double meters) {
+        Measurement measurement = new Measurement(date, meters);
+
+        database.measurementDao().insertNewMeasurement(measurement);
+    }
+}
