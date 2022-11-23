b3948ad1f966a83eb6d12dc8a2bbfe4735d2f1ed
Broxigar11 <karafadavid11@gmail.com>
2022-11-23 18:23:28 +0100

    A meresre hasznalt tengely megkeresese

--- app/app/src/main/java/net/alberlet/measurement/state/Measurement.java
+++ app/app/src/main/java/net/alberlet/measurement/state/Measurement.java
@@ -4,18 +4,47 @@ import java.util.List;
 
 public class Measurement {
 
-    public Measurement() {
+    private final List<Dimensions> measurements;
+
+    private float result;
+
+    public Measurement(List<Dimensions> measurements) {
+        this.measurements = measurements;
+    }
+
+    public void calculateResultFromMeasurements() {
+        correctMeasurementTimestamps();
+        long measuringTime = measurements.get(measurements.size()-1).getTimestamp();
 
     }
 
-    public float calculateMeasurementFromMeasurementList(List<Dimensions> measurements) {
-        return 0;
+    private int axisUsedForMeasuring(){
+        int[] axes = new int[3];
+        for(Dimensions dimensions: measurements) {
+            axes[indexOfGreatestOfThreeNumbers(dimensions.getX(), dimensions.getY(), dimensions.getZ())]++;
+        }
+        return indexOfGreatestOfThreeNumbers(axes[0], axes[1], axes[2]);
     }
 
-    private void correctMeasurementTimestamps(List<Dimensions> measurements) {
+    private void correctMeasurementTimestamps() {
         long firstTimestamp = measurements.get(0).getTimestamp();
         for(Dimensions dimensions: measurements) {
             dimensions.correctTimestamp(firstTimestamp);
         }
     }
+
+    private int indexOfGreatestOfThreeNumbers(float a, float b, float c) {
+        if(a > b){
+            if(a > c) {
+                return 1;
+            } else {
+                return 3;
+            }
+        } else if (b > c) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
 }
