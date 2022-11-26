68be0e0b82fa10ca5a1d308559abdd16090face7
l1ng4n27 <71123447+l1ng4n27@users.noreply.github.com>
2022-11-26 19:41:53 +0100

    Mérésteszt hozzáadása

--- app/app/src/test/java/net/alberlet/measurement/state/MeasurerTest.java
+++ app/app/src/test/java/net/alberlet/measurement/state/MeasurerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-class MeasurementTest {
+class MeasurerTest {
 
     @Test
     void testCalculateResultFromMeasurementsShouldReturnCorrectDistance1() {
@@ -32,5 +32,29 @@ class MeasurementTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void testCalculateResultFromMeasurementsShouldReturnCorrectDistance2() {
+        //Given
+        List<AccelerationData> accelerationDataList = List.of(
+                new AccelerationData(10f,0f,0f, 1000000000L),
+                new AccelerationData(0f,0f,0f, 2000000000L),
+                new AccelerationData(10f,0f,0f, 3000000000L),
+                new AccelerationData(10f,0f,0f, 4000000000L),
+                new AccelerationData(10f,0f,0f, 5000000000L)
+        );
+
+        Measurer testMeasurement = new Measurer(accelerationDataList);
+
+        float time = 4;
+
+        Float expected = (((time*time) * 8) / 2) * 100;
+
+        //When
+        testMeasurement.calculateResultFromMeasurements();
+        Float actual = testMeasurement.getResult();
+
+        //Then
+        Assertions.assertEquals(expected, actual);
+    }
     
 }
