df62e0cf057c6f76368d98cc8b52f3a2b71c9345
Broxigar11 <karafadavid11@gmail.com>
2022-11-26 19:49:53 +0100

    Mérés teszt negatív változóval

--- app/app/src/test/java/net/alberlet/measurement/state/MeasurerTest.java
+++ app/app/src/test/java/net/alberlet/measurement/state/MeasurerTest.java
@@ -81,5 +81,30 @@ class MeasurerTest {
         //Then
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void testCalculateResultFromMeasurementsShouldReturnCorrectDistance4() {
+        //Given
+        List<AccelerationData> accelerationDataList = List.of(
+                new AccelerationData(10f,0f,0f, 1000000000L),
+                new AccelerationData(10f,0f,0f, 2000000000L),
+                new AccelerationData(-10f,0f,0f, 3000000000L),
+                new AccelerationData(10f,0f,0f, 4000000000L),
+                new AccelerationData(10f,0f,0f, 5000000000L)
+        );
+
+        Measurer testMeasurement = new Measurer(accelerationDataList);
+
+        float time = 4;
+
+        Float expected = (((time*time) * 6) / 2) * 100;
+
+        //When
+        testMeasurement.calculateResultFromMeasurements();
+        Float actual = testMeasurement.getResult();
+
+        //Then
+        Assertions.assertEquals(expected, actual);
+    }
     
 }
